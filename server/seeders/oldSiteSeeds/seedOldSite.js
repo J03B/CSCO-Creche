da3cc1e6d4b9cb8185b6ff7a282c91cf4bfffc84
J03B <byucrazyfan@gmail.com>
2023-11-09 15:27:47 -0700

    Old site data added for users with no creche data

---
+++ server/seeders/oldSiteSeeds/seedOldSite.js
@@ -0,0 +1,41 @@
+const db = require("../../config/connection");
+const { User, Creche, Exhibit } = require("../../models");
+const crecheSeeds = require("./seedOldSiteCreches.json");
+
+db.once("open", async () => {
+  try {
+    for (let i = 0; i < crecheSeeds.length; i++) {
+      const { _id, crecheUser, yearsDonated } = await Creche.create(
+        crecheSeeds[i]
+      );
+      console.log(crecheUser);
+      const [firstName, lastName] = crecheUser.split(" ");
+      const user = await User.findOneAndUpdate(
+        { firstName, lastName },
+        {
+          $addToSet: {
+            creches: _id,
+          },
+        }
+      );
+      console.log(`User ${user.userName} has been updated with 1 creche.`);
+
+      const exhibit = await Exhibit.findOneAndUpdate(
+        { exhibitYear: yearsDonated },
+        {
+          $addToSet: {
+            creches: _id,
+          },
+        }
+      );
+      console.log(`Creche added to the ${exhibit.exhibitYear} Exhibit.`);
+    }
+    console.log("---Creches created---");
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log("all done!");
+  process.exit(0);
+});
